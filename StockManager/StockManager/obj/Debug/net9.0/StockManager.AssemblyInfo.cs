3e3e7e01e18622ad3559d26a571932d4ae1401a1
carlosafg5598 <carlosfdezvivas98@gmail.com>
2025-11-04 11:39:17 +0100

    arreglo cargar imagen

--- StockManager/StockManager/obj/Debug/net9.0/StockManager.AssemblyInfo.cs
+++ StockManager/StockManager/obj/Debug/net9.0/StockManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9274a313af5e7749729b501f115cefc8f0cfb5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3aa00cc473e534a7ebc019e90b36ac717fd6d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
